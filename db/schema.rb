60eef66ccf3dd1b9b6976efc54b9102ce4436fe1
Serena Chen <serenasyue@gmail.com>
2015-09-25 22:56:03 -0400

    Added bootstrap.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,91 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926002408) do
+ActiveRecord::Schema.define(version: 20150926003659) do
+
+  create_table "accommodations", force: true do |t|
+    t.string   "name"
+    t.datetime "check_in_time"
+    t.datetime "check_out_time"
+    t.integer  "group_id"
+    t.boolean  "is_final_plan"
+    t.integer  "cost"
+    t.string   "longitude"
+    t.string   "latitude"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "accomodation_users", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "accomodation_id"
+    t.boolean  "like"
+    t.boolean  "dislike"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "event_users", force: true do |t|
+    t.integer  "event_id"
+    t.integer  "user_id"
+    t.boolean  "like"
+    t.boolean  "dislike"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.integer  "schedule_id"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "description"
+    t.integer  "cost"
+    t.string   "longitude"
+    t.string   "location_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "group_users", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "groups", force: true do |t|
     t.string   "name"
     t.integer  "group_admin_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "schedules", force: true do |t|
+    t.integer  "group_id"
+    t.datetime "date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "transportation_users", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "transporation_id"
+    t.boolean  "like"
+    t.boolean  "dislike"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "transportations", force: true do |t|
+    t.integer  "group_id"
+    t.boolean  "is_final_plan"
+    t.boolean  "like"
+    t.boolean  "dislike"
+    t.string   "departure_location"
+    t.string   "arrival_location"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
